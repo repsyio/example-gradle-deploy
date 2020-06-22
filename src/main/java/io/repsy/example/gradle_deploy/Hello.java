c8e82d02c177effb44004e62caa162918401374e
firatkucuk <firat@fria.io>
2020-06-22 22:45:23 +0200

    initial commit

---
+++ src/main/java/io/repsy/example/gradle_deploy/Hello.java
@@ -0,0 +1,9 @@
+package io.repsy.example.gradle_deploy;
+
+public class Hello {
+
+    public static String sayHello(final String name) {
+
+        return "Hello " + name + ", Greetings from repsy.io";
+    }
+}
